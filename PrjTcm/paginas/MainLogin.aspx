7275ce3bd514d7ea65485b2b6d60846646afe06a
Henrique Mantovani <henriquepio29@gmail.com>
2025-11-19 23:15:05 -0300

    login

---
+++ PrjTcm/paginas/MainLogin.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MainLogin.aspx.cs" Inherits="PrjTcm.paginas.MainLogin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link  rel="stylesheet" type="text/css" href="../css/login.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="painel">
+            <h1>Login</h1>
+            <asp:Label ID="lblMensagem" runat="server" Text=""></asp:Label>
+            <div class="painel-section">
+                <h2>Nome:</h2>
+                <asp:TextBox ID="txtNome" runat="server"></asp:TextBox>
+            </div>
+            <div class="painel-section">
+                <h2>Senha:</h2>
+                <asp:TextBox ID="txtSenha" runat="server" TextMode="Password"></asp:TextBox>
+            </div>
+            <asp:Button ID="btnEntrar" runat="server" Text="Entrar" CssClass="painel-btn" OnClick="btnEntrar_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ PrjTcm/paginas/MainLogin.aspx.cs
@@ -0,0 +1,55 @@
+﻿using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Common;
+using MySqlX.XDevAPI.Relational;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PrjTcm.paginas
+{
+    public partial class MainLogin : System.Web.UI.Page
+    {
+        Funcoes f = new Funcoes();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnEntrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MySqlCommand comando = new MySqlCommand();
+                comando.CommandText =
+                    "SELECT * FROM tblUsuario WHERE nome = @usuario AND senha = @senha";
+
+                string usuario = txtNome.Text;
+                string senha = txtSenha.Text;
+
+                comando.Parameters.AddWithValue("@usuario", usuario);
+                comando.Parameters.AddWithValue("@senha", senha);
+
+                DataTable dt = f.exSQLParameters(comando);
+
+                if (dt != null && dt.Rows.Count > 0)
+                {
+                    Session["usuario"] = dt.Rows[0]["nome"].ToString();
+                    Response.Redirect("painel.aspx");
+                }
+                else
+                {
+                    lblMensagem.Text = "Usuário ou senha inválidos.";
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMensagem.Text = "Erro ao efetuar login: " + ex.Message;
+            }
+        }
+    }
+}
