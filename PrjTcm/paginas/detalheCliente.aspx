bbf2470fcdacbc37ba51d6b079e07810c9d0aef0
Gustavo <gustavo.ggimenez03@gmail.com>
2025-11-20 14:45:14 -0300

    adicionei pagina de detelhes cliente

---
+++ PrjTcm/paginas/detalheCliente.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/paginas/masters/mestra.Master" AutoEventWireup="true" CodeBehind="detalheCliente.aspx.cs" Inherits="PrjTcm.paginas.masters.detalhePedido" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    <link rel="stylesheet" href="../../css/detalhe.css" />
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+<main>
+    <div class="painel">
+        <div class="titulo-painel"><h1>Clientes</h1></div>
+
+            <div class="campos-painel">
+                <div class="coluna">
+                    <div class="campo"><h2>Nome:</h2><asp:TextBox ID="txtNomeCliente" runat="server"></asp:TextBox></div>
+                    <div class="campo"><h2>CNPJ:</h2><asp:TextBox ID="txtCnpjCliente" runat="server"></asp:TextBox></div>
+                    <div class="campo"><h2>Celular:</h2><asp:TextBox ID="TextBox2" runat="server"></asp:TextBox></div>
+                    <div class="campo"><h2>Email:</h2><asp:TextBox ID="TextBox3" runat="server"></asp:TextBox></div>
+                </div>
+                <div class="coluna">
+                    <div class="campo"><h2>Logradouro:</h2><asp:TextBox ID="TextBox4" runat="server"></asp:TextBox></div>
+                    <div class="campo"><h2>Número:</h2><asp:TextBox ID="TextBox5" runat="server"></asp:TextBox></div>
+                    <div class="campo"><h2>Cidade:</h2><asp:TextBox ID="TextBox6" runat="server"></asp:TextBox></div>
+                    <div class="campo"><h2>Bairro:</h2><asp:TextBox ID="TextBox7" runat="server"></asp:TextBox></div>
+                    <div class="campo"><h2>UF:</h2><asp:TextBox ID="TextBox8" runat="server"></asp:TextBox></div>
+                    <div class="campo"><h2>CEP:</h2><asp:TextBox ID="TextBox9" runat="server"></asp:TextBox></div>
+                </div>
+                <div class="coluna">
+
+                </div>
+            </div>
+
+            <div class="botoes-painel">
+                <asp:Button ID="Button1" CssClass="confirmar" runat="server" Text="Confirmar" />
+                <asp:Button ID="Button2" CssClass="cancelar" runat="server" Text="Cancelar" />
+            </div>
+    </div>
+</main>
+    
+</asp:Content>
---
+++ PrjTcm/paginas/detalheCliente.aspx.designer.cs
@@ -0,0 +1,125 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Esse código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace PrjTcm.paginas.masters
+{
+
+
+    public partial class detalhePedido
+    {
+
+        /// <summary>
+        /// Controle txtNomeCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNomeCliente;
+
+        /// <summary>
+        /// Controle txtCnpjCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtCnpjCliente;
+
+        /// <summary>
+        /// Controle TextBox2.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+
+        /// <summary>
+        /// Controle TextBox3.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+
+        /// <summary>
+        /// Controle TextBox4.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+
+        /// <summary>
+        /// Controle TextBox5.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox5;
+
+        /// <summary>
+        /// Controle TextBox6.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox6;
+
+        /// <summary>
+        /// Controle TextBox7.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox7;
+
+        /// <summary>
+        /// Controle TextBox8.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox8;
+
+        /// <summary>
+        /// Controle TextBox9.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox9;
+
+        /// <summary>
+        /// Controle Button1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Controle Button2.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Para modificar, mova a declaração de campo do arquivo de designer a um arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
+    }
+}
