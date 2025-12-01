aa07f71ea622f893f9072d5d056233f6cede6e57
Gustavo <gustavo.ggimenez03@gmail.com>
2025-12-01 18:55:44 -0300

    menu pra celular e js (só falta a responsiviade dos grids)

---
+++ PrjTcm/scripts/menu.js
@@ -0,0 +1,15 @@
+﻿function alternarMenu() {
+    const menuLateral = document.getElementById("menu-lateral");
+    menuLateral.classList.toggle("ativo");
+}
+
+document.addEventListener("click", function (event) {
+    const menuLateral = document.getElementById("menu-lateral");
+    const botaoMenu = document.querySelector(".botao-menu");
+
+    if (menuLateral.classList.contains("ativo") &&
+        !menuLateral.contains(event.target) &&
+        !botaoMenu.contains(event.target)) {
+        menuLateral.classList.remove("ativo");
+    }
+});
